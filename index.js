447551992071e47e6cd36eec39246fdef6878e42
Marquise <marquise_dawkins@aol.com>
2024-03-05 18:40:51 -0500

    button disappear complete

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ var minuss = document.getElementById("minus");
 function hide(){  
    var getMe = minuss.style.display = "none";
 
-   if (getMe){
-    document.getElementById("hideMe").style.display = "none";
-   }else{
-    document.getElementById("showMe").style.display = "block";
-   }
+   document.getElementById("showMe").style.display = "block";
+
+   document.getElementById("hideMe").style.display = "none";
+
 }
 
 function show(){
     var showSum = minuss.style.display = "block";
 
     document.getElementById("hideMe").style.display = "block";
+
+    document.getElementById("showMe").style.display = "none";
 }
 
 
