8025cfdf643af25c17a152f48b41951d4838b5cb
Marquise <marquise_dawkins@aol.com>
2024-03-06 23:35:49 -0500

    name update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var minuss = document.getElementById("minus");
+var paragraphOne = document.getElementById("answer-one");
 
 
 
 
 function hide(){  
-   var getMe = minuss.style.display = "none";
+   var getMe = paragraphOne.style.display = "none";
 
    document.getElementById("showMe").style.display = "block";
 
@@ -13,7 +13,7 @@ function hide(){
 }
 
 function show(){
-    var showSum = minuss.style.display = "block";
+    var showSum = paragraphOne.style.display = "block";
 
     document.getElementById("hideMe").style.display = "block";
 
